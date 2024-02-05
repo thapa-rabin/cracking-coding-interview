b924ef998be196b9c9f437d677ef9cd2ec73a770
Rabin Thapa <thapa.rbn24@gmail.com>
2024-02-05 17:18:13 -0500

    Update Solution and Test for 1.1

--- Chapter-1/1.1/Solution.java
+++ Chapter-1/1.1/Solution.java
@@ -10,7 +10,7 @@
      *                  Can also argue as O(1) or O(c) or O(min(c,n)) where c is size of character set
      * Space Complexity: O(1) or O(c) 
      */
-    public boolean isUniqueChars(String str) {
+    public boolean isUniqueCharsUsingBooleanArray(String str) {
         // This solution assumes input string contains character among 128 ASCII characters
         int N = 128;
         boolean [] char_set = new boolean[N];
@@ -31,18 +31,20 @@
 
     /*
      * Solution 2: Using bit vector
+     * This solution assumes the input string contains only lowercase alphabets. Considering all chars will result in BIG bit vector (int).
+     * Use TestResults1.txt and TestResults1.txt for testing since this solution has restrictive assumptions
      * This reduces space complexity by a factor of 8
      */
-    public boolean isUniqueCharsUsingBits(String str) {
+    public boolean isUniqueCharsUsingBitVector(String str) {
         int bitVector = 0;
 
         for(int i = 0; i < str.length() ;  i++) {
-            int val = 1 << str.charAt(i);
+            int val = 1 << (str.charAt(i) - 'a');
             if((bitVector & val) > 0) { // happens when char is repeated
                 return false;
             }
             // else track the char in bit vector
-            bitVector += bitVector | 1 << str.charAt(i);
+            bitVector |= 1 << (str.charAt(i) - 'a');
 
         }
         return true;
@@ -54,6 +56,5 @@
      * ii. Sort the string and iterate to check if any pair of adjacent chars are same. Time Complexity O(nlogn).
      *     But some sorting algorithm (ex: Merge sort) uses additional space
      */
-
-
+    
  }
--- Chapter-1/1.1/Test.java
+++ Chapter-1/1.1/Test.java
@@ -1,40 +1,62 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws FileNotFoundException {
-        // files for TestCases and ExpectedResults
-        File testFile = new File("TestCases.txt");
-        File resultFile = new File("TestResults.txt");
+        // Checking method isUniqueCharsUsingBooleanArray
+        System.out.println("For Solution 1 -");
+        checkSolution(1, 
+                        scanFile(new File("TestCases1.txt")), 
+                        scanFile(new File("TestResults1.txt"))
+                    );
 
-        // surround with try to prevent resource leak
-        try (Scanner scTest = new Scanner(testFile)) {
-            try (Scanner scResult = new Scanner(resultFile)) {
-                int testCounter = 0;
-
-                while(scTest.hasNextLine() && scResult.hasNextLine()) {
-                    String testStr = scTest.nextLine();
-                    String resultStr = scResult.nextLine();
-
-                    System.out.print("Checking Test Case " + ++testCounter + ":");
-                    checkSolution(testStr, resultStr);
-                }
-            }
-        }
-
+        // Checking method isUniqueCharsUsingBitVector
+        System.out.println("For Solution 2 -");
+        checkSolution(1, 
+                        scanFile(new File("TestCases2.txt")), 
+                        scanFile(new File("TestResults2.txt"))
+                    );
     }
 
-    // checks answer against the provided result
-    public static void checkSolution(String test, String result) {
-        Solution solution = new Solution();
+    public static void checkSolution(int solutionNum, ArrayList<String> test, ArrayList<String> result) {
+        if(test.size() == result.size()) {
+            Solution solution = new Solution();
+            String answer = null;
+            switch (solutionNum) {
+                case 1:
+                    for(int i = 0; i < test.size(); i++) {
+                        System.out.print("Checking Test Case " + (i+1) + ":");
+                        answer = solution.isUniqueCharsUsingBooleanArray(test.get(i)) ? "True" : "False";
+                        System.out.println(result.get(i).equalsIgnoreCase(answer) ? "Correct!" : "Incorrect!");
+                    }
+                    break;
+                case 2:
+                    for(int i = 0; i < test.size(); i++) {
+                        System.out.print("Checking Test Case " + (i+1) + ":");
+                        answer = solution.isUniqueCharsUsingBitVector(test.get(i)) ? "True" : "False";
+                        System.out.println(result.get(i).equalsIgnoreCase(answer) ? "Correct!" : "Incorrect!");
+                    }
+                    break;
+                default:
+                    break;
+            }  
+        }
+    }
 
-        // Uncomment a method to test
-        // Solution 1
-        //String answer = solution.isUniqueChars(test) ? "True" : "False";
-        // Solution 2
-        String answer = solution.isUniqueChars(test) ? "True" : "False";
-
-        System.out.println(result.equalsIgnoreCase(answer) ? "Correct!" : "Incorrect!");
+    // reads TestCases and TestResults files
+    public static ArrayList<String> scanFile(File file){
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()) {
+                list.add(sc.nextLine());
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.print("File Not Found!");
+        }
+        return list;
     }
 }
